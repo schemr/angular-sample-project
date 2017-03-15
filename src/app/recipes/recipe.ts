9f751a2be2679c5149c3a5eaa566d7c7ec60a7ca
schemr <schemryj@gmail.com>
2017-03-15 21:20:58 +0900

    type 추가

--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -1,5 +1,5 @@
 import { Ingredient } from '../shared/ingredient'
 
 export class Recipe {
-    constructor(public name, public description, public imagePath, public ingredient: Ingredient[]) {}
+    constructor(public name: string, public description: string, public imagePath: string, public ingredient: Ingredient[]) {}
 }
