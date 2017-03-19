5f6d364e479f4719e615b30aac71aa7383501839
schemr <schemryj@gmail.com>
2017-03-19 19:52:15 +0900

    Recipe Detail 라우트 파라미터를 통한 데이터 전달 형태로 변경

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { RecipeService } from '../recipe.service';
+import { Subscription } from 'rxjs/Rx';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../recipe';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 
@@ -6,12 +9,25 @@ import { ShoppingListService } from '../../shopping-list/shopping-list.service';
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe: Recipe;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private recipeIndex: number;
+  selectedRecipe: Recipe;
 
-  constructor(private sls:ShoppingListService) { }
+  constructor(private sls:ShoppingListService, 
+              private route: ActivatedRoute,
+              private recipesService: RecipeService) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.recipeIndex = params['id'];
+        this.selectedRecipe = this.recipesService.getRecipe(this.recipeIndex);
+      }
+    )
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
   onAddToShoppingList(){
     this.sls.addItems(this.selectedRecipe.ingredient);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -17,5 +17,7 @@ export class RecipeService {
   getRecipes(){
     return this.recipes;
   }
-
+  getRecipe(id: number){
+    return this.recipes[id];
+  }
 }
