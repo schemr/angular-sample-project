289ebbe10d08141cb1af36f73f29a165cbd122d0
schemr <schemryj@gmail.com>
2017-03-19 20:12:18 +0900

    output, eventEmitter 제거, 헤더 네비게이션 active 추가(routerLinkActive)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe';
 import { RecipeService } from '../recipe.service';
 
@@ -9,7 +9,7 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
 
-  @Output() recipeSelected = new EventEmitter<Recipe>();
+  //@Output() recipeSelected = new EventEmitter<Recipe>();
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
