641d3367d1815a178c092e923541631d1066ba4b
schemr <schemryj@gmail.com>
2017-03-21 11:40:13 +0900

    Select Item 초기값 처리

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -15,6 +15,7 @@ export class ShoppingListAddComponent implements OnChanges {
   ngOnChanges(changes) {
     if(changes.item.currentValue === null){
       this.isAdd = true;
+      this.item = {name:null, amount:null};
     }else{
       this.isAdd = false;
     }
