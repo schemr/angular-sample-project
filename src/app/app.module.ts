5b1a50ff7cc2f5e3773156cf50976b3e268ab703
schemr <schemryj@gmail.com>
2017-04-05 21:46:23 +0900

    Shared Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { RecipesModule } from './recipes/recipes.module';
-import { AppRoutingModule } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SharedModule } from './shared/shared.module';
+import { RecipesModule } from './recipes/recipes.module';
+import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
-import { DropdownDirective } from './dropdown.directive';
 
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -20,14 +20,14 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListAddComponent,
-    DropdownDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    RecipesModule
+    RecipesModule,
+    SharedModule
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
