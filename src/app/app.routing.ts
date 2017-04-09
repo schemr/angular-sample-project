f75fbcce06fc9fecb8809876b725b1feddb00f95
schemr <schemryj@gmail.com>
2017-04-09 19:57:56 +0900

    Signin 구현

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 const APP_ROUTES: Routes = [
     { path: '', component: SignupComponent},
     { path: 'shopping-list', component: ShoppingListComponent},
-    { path: 'signup', component: SignupComponent}
+    { path: 'signup', component: SignupComponent},
+    { path: 'signin', component: SigninComponent}
 ]
 
 @NgModule({
