f8b8ce24700e43cb3425be45cba6bbee5a28d9a8
schemr <schemryj@gmail.com>
2017-04-09 18:47:32 +0900

    Header 폴더로 분리

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { DataStorageService } from '../shared/data-storage.service';
+
+@Component({
+  selector: 'rb-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) { }
+  
+  onStore(){
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        data => console.log(data),
+        error => console.error(error)
+    );
+  }
+  onFetch(){
+    this.dataStorageService.getRecipes();
+  }
+}
