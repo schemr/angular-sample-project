6881f933fba645ff58b0dd775908cb580352dbb9
schemr <schemryj@gmail.com>
2017-12-20 18:52:11 +0900

    Add logout router navigation(ngrx effect)

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -53,7 +53,12 @@ export class AuthEffects {
                 }
             ];
         })
-
+    @Effect({dispatch: false})
+    authLogout = this.actions$
+        .ofType(AuthActions.LOGOUT)
+        .do(() => {
+            this.router.navigate(['/'])
+        })
     constructor(private actions$: Actions, private router: Router) {}
 
 }
